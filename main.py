6a5cfeccb19843c71042f9d852ada97641bad848
agiachris <c.agia@yahoo.com>
2019-01-19 01:50:48 -0500

    hope complete

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ from parser import *
 from sim import *
 
 if __name__ == '__main__':
+
+
 	beds = [100, 100, 100]
 	hospital = make_hospital(len(beds), beds)
 	patient_queue = build_queue('data.txt')
+
 	parse(patient_queue)
+
+	curr_time = 0  # Minute-by-minute timestamp of the simulation
+	metadata = []  # Metadata will store the simulations performance metrics
+	for i in range(18):
+		metadata.append(0)
+
+	curr_time = sim(hospital, patient_queue, metadata, curr_time)
+	final_statistics(metadata, hospital, curr_time)
--- sim.py
+++ sim.py
@@ -200,16 +200,5 @@ def wait_to_priority(wait, priority):
         return 4
 
 
-if __name__ == "__main__":
-    beds = [100, 100, 100]
-    hospital = make_hospital(len(beds), beds)
-    patient_queue = build_queue('data.txt')
 
-    curr_time = 0  # Minute-by-minute timestamp of the simulation
-    metadata = []  # Metadata will store the simulations performance metrics
-    for i in range(18):
-        metadata.append(0)
-
-    curr_time = sim(hospital, patient_queue, metadata, curr_time)
-    final_statistics(metadata, hospital, curr_time)
 
