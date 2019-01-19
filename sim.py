9a893eeebb90f8a2b0593248b953f8d7ba2a4dbc
UTATavionics <33789764+UTATavionics@users.noreply.github.com>
2019-01-18 22:04:51 -0500

    add
    added data file for testing & buildqueue function

--- sim.py
+++ sim.py
@@ -1,1 +1,24 @@
+from util import *
 
+
+def build_queue(filename):
+    patient_queue = PatientQueue()
+
+
+    # import data from a file
+    f = open(filename, "r")
+    lines = f.readlines()
+    f.close()
+    lines = [line.rstrip('\n') for line in lines]
+
+
+    for line in lines:
+        line = line.split(",")
+        patient_queue.insert(Patient(line[0], line[1], line[2], line[3], -1, 0, line[6], line[7], line[8], line[9], line[10], line[11], line[12], line[13]))
+
+    return patient_queue
+
+
+if __name__ == '__main__':
+    patient_queue = build_queue("data.txt")
+    print(patient_queue.pop().name)
